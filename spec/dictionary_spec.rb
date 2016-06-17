cfe540b919d7e13fa6e2c6ed649c607b4181bd2d
Joshua Lane <me@joshualane.com>
2016-06-17 14:48:23 -0700

    test(dictionary): add create example

---
+++ spec/dictionary_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+RSpec.describe 'Dictionary' do
+  it 'creates a dictionary' do
+    service = a_service
+
+    name = SecureRandom.hex(3)
+    expect(
+      service.dictionaries.create(name: name).identity
+    ).not_to be_nil
+  end
+
+  #describe 'with a dictionary' do
+    #let!(:dictionary) { a_dictionary(locked: false) }
+    #let!(:service) { dictionary.service }
+
+    #it 'fetches the dictionary' do
+      #expect(client.dictionaries(service_id: dictionary.service_id).get(dictionary.identity)).to eq(dictionary)
+    #end
+
+    #it 'lists dictionaries' do
+      #expect(client.dictionaries(service_id: a_dictionary.service_id)).to include(dictionary)
+    #end
+
+    #it 'updates a dictionary' do
+      #comment = SecureRandom.hex(6)
+
+      #dictionary.update(comment: comment)
+
+      #expect(dictionary.comment).to eq(comment)
+      #expect(dictionary.reload.comment).to eq(comment)
+    #end
+  #end
+end
