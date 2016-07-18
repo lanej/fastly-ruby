343ee2a5eb574b07b1f3dcfd49b8254a90662400
Joshua Lane <me@joshualane.com>
2016-07-18 11:31:22 -0700

    test(*): setup mock/real example filters

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,16 @@ require 'fastly'
 Dir[File.expand_path('../{support,shared,matchers,fixtures}/*.rb', __FILE__)].each { |f| require(f) }
 
 Fastly.mock! if ENV['MOCK_FASTLY'] == 'true'
+
+RSpec.configure do |config|
+  if Fastly.mocking?
+    config.before(:each) { Fastly.reset! }
+    config.filter_run_excluding(real: true)
+  else
+    config.filter_run_excluding(mock: true)
+  end
+  config.filter_run(focus: true)
+  config.run_all_when_everything_filtered = true
+
+  config.order = 'random'
+end
