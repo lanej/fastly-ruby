2d72b89ba43a098e4461cc94422e7480fa053f26
Joshua Lane <me@joshualane.com>
2016-08-10 12:59:47 -0700

    test(model): fix version diff setup
    resource helper is not producing different versions, even though i asked
    nicely.
    
    get a valid version and clone! to ensure different resources

--- spec/diff_spec.rb
+++ spec/diff_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 
 RSpec.describe Fastly do
   let!(:service) { a_service }
-  let!(:version_a) { a_version(service: service) }
-  let!(:version_b) { a_version }
+  let!(:version_a) { a_version(service: service, locked: false) }
+  let!(:version_b) { version_a.clone! }
 
   it 'diffs two versions' do
     diff = version_a.diff(version_b)
